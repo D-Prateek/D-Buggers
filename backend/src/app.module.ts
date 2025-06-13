0c33ff76843ede12c4b334f618fff917b4c7de00
n3vilreal <nebil.241423@ncit.edu.np>
2025-06-13 12:30:43 +0545

    adding moongose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+    'mongodb+srv://n3vilreal:AamaCareCluster@aamacare-cluster.7dgof76.mongodb.net/?retryWrites=true&w=majority&appName=AamaCare-Cluster'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
